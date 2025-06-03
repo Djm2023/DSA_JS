41515d4648961af49eea1a23270a9f7f6af7a913
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2025-06-03 21:06:31 +0530

    learnt about functions.

---
+++ Introduction - 01/functions.js
@@ -0,0 +1,13 @@
+// 1. Write a function to add two numbers.
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(5,6));
+
+// 2. Write a function to find squares of a numbers.
+function square(a){
+    return a*a;
+}
+
+console.log(square(5));
